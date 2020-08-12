b3a92f027ab1d0f7308252eaed3c2a0dfa804d51
sotirm <sotirm@umich.edu>
2020-08-12 11:54:55 -0500

    submission for deadline

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,18 +1,54 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from "../components/BotSpecs";
+
+
 
 class BotCollection extends React.Component {
-  //your code here
+  
+
+	state = {
+		currentBot: {}
+	}
+
+	setCurrentBot = (bot) => {
+		this.setState({
+		  currentBot: bot
+		});
+	  };
+
+
+	  clearCurrentBot = () => {
+		this.setState({
+		  currentBot: {}
+		});
+	  };
+
+
+	  renderBots = () => {
+		if (!this.state.currentBot.id) {
+		  return (
+			<div className="row">
+			  {this.props.botList.map(bot => (
+				<BotCard key={bot.id} bot={bot} viewBot={this.setCurrentBot} enList={this.props.enList} />
+			  ))}
+			</div>
+		  );
+		} else {
+		  return (
+			<BotSpecs
+			  bot={this.state.currentBot}
+			  enlist={this.props.enList}
+			  myForce={this.props.myForce}
+			  goBack={this.clearCurrentBot}
+			/>
+		  );
+		}
+	  };
+	
 
   render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+	return <div className="ui four column grid">{this.renderBots()}</div>;
   }
 
 };
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,13 +1,52 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from "./YourBotArmy";
+import BotSpecs from "../components/BotSpecs";
+
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+
+
+  state = {
+    botList: [],
+    myBots: [],
+    myForce: []
+  }
+
+  componentDidMount(){
+
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots').then(res => res.json()).then(bots => this.setState({botList: bots}))
+  }
+
+  enList = (botId) => {
+
+    this.setState({...this.state.myBots.push(botId)})
+    
+  };
+
+  moreDetails = () => {
+
+    console.log("hello")
+
+  }
+
+  myForce = (bot) => {
+    this.setState({myForce: [...this.state.myForce, bot]})
+  }
+
 
   render() {
     return (
+  <div>
       <div>
-        {/* put your components here */}
+        {<YourBotArmy botList={this.state.botList} myBots={this.state.myBots} />}
       </div>
+
+      <div>
+        {<BotCollection botList={this.state.botList} enList={this.enList} myBots={this.state.myBots} myForce={this.myForce}/> }
+      </div>
+  </div>
     );
   }
 
