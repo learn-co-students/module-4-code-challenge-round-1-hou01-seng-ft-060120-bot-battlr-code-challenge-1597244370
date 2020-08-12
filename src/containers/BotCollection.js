8f1fe814c0a55c15b15ffe1b9750642ac3c0785d
Julio <julio.chazari@gmail.com>
2020-08-12 11:54:16 -0500

    submission for deadline

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -8,8 +8,13 @@ class BotCollection extends React.Component {
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.props.bots.map(bot => (
+				<BotCard
+					key={bot.id}
+					bot={bot}
+					handleBotClick={this.props.handleBotClick}
+				/>
+			  ))}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,80 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+import BotSpecs from "../components/BotSpecs";
+
+const BOTS_URL = "https://bot-battler-api.herokuapp.com/api/v1/bots";
+const defaultView = { display: false, bot: null }
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  state = {
+    bots: [],
+    botArmy: [],
+    botSpecs: defaultView
+  }
+  
+  componentDidMount() {
+    fetch(BOTS_URL)
+      .then(res => res.json())
+      .then(bots => {
+        this.setState({ bots })
+      })
+  }
+
+  handleBotClick = (bot) => {
+    this.setState({
+      botSpecs: {
+        display: true, 
+        bot: bot
+      }
+    })
+  }
+
+  handleBackBtn = () => {
+    this.setState({ 
+      botSpecs: defaultView
+    })
+  }
+
+  handleEnlistBtn = (bot) => {
+    const botArmy = [ ...this.state.botArmy ]
+
+    if(!botArmy.includes(bot)) {
+      botArmy.push(bot);
+    } else if(botArmy.includes(bot)) {
+      alert(`${bot.name} is already enlisted!`)
+    }
+
+    this.setState({ botArmy, botSpecs: defaultView })
+  }
+
+  unEnlistBot = (bot) => {
+    const botArmy = this.state.botArmy.filter(botty => botty !== bot)
+    this.setState({ botArmy })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy
+          botArmy={this.state.botArmy}
+          handleBotClick={this.handleBotClick}
+          unEnlistBot={this.unEnlistBot}
+        />
+        { this.state.botSpecs.display
+            ?
+          <BotSpecs
+            bot={this.state.botSpecs.bot}
+            handleBackBtn={this.handleBackBtn}
+            handleEnlistBtn={this.handleEnlistBtn}
+          />
+            :
+          <BotCollection
+            bots={this.state.bots}
+            handleBotClick={this.handleBotClick}
+          />
+        }
       </div>
     );
   }
