abfa46f2ffebe31b903e7756188fa32bffea3e82
github username <github email address>
2020-08-12 12:10:22 -0500

    done

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,65 @@
 import React from "react";
+import BotCollection from "./BotCollection.js";
+import YourBotArmy from "./YourBotArmy.js";
+import BotSpecs from "../components/BotSpecs.js";
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+	//start here with your code for step one
+	state = {
+		bots: "",
+		army: "",
+		cliked: false,
+		botSpecs: ""
+  };
 
-  render() {
-    return (
-      <div>
-        {/* put your components here */}
-      </div>
-    );
-  }
+  componentDidMount() {
+		fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+			.then(res => res.json())
+			.then(bots => {
+				this.setState({
+					bots: bots
+				});
+			});
+	}
 
+  
+  isClicked = bot => {
+		this.setState({
+			cliked: !this.state.cliked,
+			botSpecs: bot
+		});
+	};
+
+
+	getBots = yourBot => {
+		if (this.state.army.includes(yourBot)) {
+			this.setState({
+				army: this.state.army.filter(bot => bot !== yourBot)
+			});
+		} else {
+			this.setState({
+				army: [...this.state.army, yourBot]
+			});
+		}
+	};
+
+
+	render() {
+		return (
+			<div>
+				<YourBotArmy yourBots={this.state.army} army={this.getBots} />
+				{!this.state.cardCliked ? (
+					<BotCollection allBots={this.state.bots} clicked={this.isClicked} />
+				) : (
+					<BotSpecs
+						bot={this.state.botSpecs}
+						clicked={this.isClicked}
+						army={this.getBots}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
 export default BotsPage;
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,22 +1,29 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotCollection from "./BotCollection";
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+	//your bot army code here...
 
-  render(){
-    return (
-      <div className="ui segment inverted olive bot-army">
-        <div className="ui five column grid">
-          <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
-          </div>
-        </div>
-      </div>
-    );
-  }
-  
-};
+	handleClick = e => {
+		this.props.army(e);
+	};
+
+	render() {
+		return (
+			<div className="ui segment inverted olive bot-army">
+				<div className="ui five column grid">
+					<div className="row bot-army-row">
+						{this.props.yourBots
+							? this.props.yourBots.map(bot => {
+									return <BotCard bot={bot} armyBot={this.handleClick} />;
+							  })
+							: null}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default YourBotArmy;
