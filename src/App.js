2c62a4896aa72da19929156e13a403e757b6aa65
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-08-12 11:54:11 -0500

    submission for deadline

--- src/App.js
+++ src/App.js
@@ -3,10 +3,55 @@ import BotsPage from "./containers/BotsPage";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bots: [],
+      enlisted: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchBots()
+  }
+
+  fetchBots() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then(bots => this.setState({bots: bots}))
+  }
+
+  enlist = (bot) => {
+    if (this.state.enlisted.includes(bot)) {
+      console.log('Sorry, this robot is already on your army and I wont let you add it again because I need to pass this code challenge, baby')
+    } else {
+      this.setState({
+        enlisted: [...this.state.enlisted, bot]
+      })
+    }
+  }
+
+  remove = (mybot) => {
+    let oldEnlistedArray = this.state.enlisted
+    let index = oldEnlistedArray.indexOf(mybot)
+    if (index > -1) {
+      oldEnlistedArray.splice(index, 1);
+    }
+    this.setState({
+      enlisted: oldEnlistedArray
+    })
+    console.log(mybot)
+    console.log(this.state.enlisted)
+    this.fetchBots()
+  }
   render() {
     return (
       <div className="App">
-        <BotsPage />
+        <BotsPage 
+        bots={this.state.bots}
+        enlist={this.enlist}
+        enlistedBots={this.state.enlisted}
+        remove={this.remove}/>
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -4,13 +4,24 @@ import BotCard from "../components/BotCard";
 class YourBotArmy extends React.Component {
   //your bot army code here...
 
+  state = {
+    iAmEnlisted: true
+  }
+
+  // clickBot = (clickedBot) => {
+  //   if (this.props.enlistedBots.includes(clickedBot)) {
+  //     this.props.remove(this.props.bot)
+  //   } else {
+  //     this.props.enlist(this.props.bot)
+  //   }
+	//   }
+
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.props.enlistedBots.map(bot => <BotCard bot={bot} key={bot.id} taken={this.state.iAmEnlisted} remove={this.props.remove} clickBot={this.props.clickBot}/>)}
           </div>
         </div>
       </div>
